21fd0af6c5fed108eb49440ef3b636bf89ff44f2
wujishanxia <tianshibala@gmail.com>
2017-08-11 22:45:41 +0800

    Initial Commit

---
+++ c语言/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c语言
+//
+//  Created by wujishanxia on 2017/8/11.
+//  Copyright © 2017年 wujishanxia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
